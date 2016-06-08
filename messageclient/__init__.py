7cf105deacd9f7e3288de81f1ae923396e3c9f57
徐应全 <yingquan.xu@shatacloud.com>
2016-06-08 16:39:13 +0800

    modified by xyq at Wed Jun  8 16:39:13 CST 2016

--- messageclient/__init__.py
+++ messageclient/__init__.py
@@ -26,7 +26,7 @@ LOG = util.init_logger('messageclient', '/var/log/messageclient.log')
 from messageclient.rabbitmq_driver.rabbit_engine import PikaEngine, Target, Transport
 from messageclient.rabbitmq_driver.rabbit_engine import get_transport
 from messageclient.rabbitmq_driver.rabbit_message import Message
-from messageclient.rabbitmq_driver import Consumer, Publisher
+from messageclient.rabbitmq_driver import Consumer, Publisher, RpcPublisher, RpcConsumer
 
 
 event = threading.Event()           # use for protect global variable g_result
