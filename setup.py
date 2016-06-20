35d876a13b5d293d81e88f3e281fe82285c8b50c
徐应全 <yingquan.xu@shatacloud.com>
2016-06-20 13:49:48 +0800

    modified by xyq at Mon Jun 20 13:49:48 CST 2016

--- setup.py
+++ setup.py
@@ -9,28 +9,17 @@
 
 
 from distutils.core import setup
-import os
-
-name_prefix = os.environ.get('NAME_PREFIX', '')
-software_name = os.environ.get('SOFTWARE_NAME', '')
-version = os.environ.get('VERSION', '')
-batch_num = os.environ.get('BUILD_NUMBER', '0')
 
 
-if software_name != '':
-    name = name_prefix + software_name
-else:
-    name = "shata-messageclient"
+SOFTWARE_NAME = "shata-messageclient"
+VERSION = '1.3.2'
+URL = "http://www.shatacloud.com/"
 
-if version != '':
-    version = '%s.%s' % (version, batch_num)
-else:
-    version = '1.3.2'
 
 setup(
-    name=name,
-    version=version,
-    url="http://www.shatacloud.com/",
+    name=SOFTWARE_NAME,
+    version=VERSION,
+    url=URL,
     packages=["messageclient", "messageclient.rabbitmq_driver"],
     scripts=[]
 )
